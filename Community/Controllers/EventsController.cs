70d1bda8a0a19b4142e695ba2e08115ba626dfe3
jellar <jella.rajesh@gmail.com>
2017-11-10 19:00:34 +0000

    initial commt

---
+++ Community/Controllers/EventsController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Community.Controllers
+{
+    public class EventsController : Controller
+    {
+        // GET: Events
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Events
+        public ActionResult Gallery()
+        {
+            return View();
+        }
+    }
+}
